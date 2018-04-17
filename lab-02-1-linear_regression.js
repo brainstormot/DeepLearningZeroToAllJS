941622e5c72d1d590a937c67800e4374098db654
brainstormot <kanghyr@gmail.com>
2018-04-17 16:46:21 +0900

    billboard.js addedcdx

--- lab-02-1-linear_regression.js
+++ lab-02-1-linear_regression.js
@@ -43,25 +43,73 @@ optimizer = tf.train.sgd(learning_rate)
 // train = optimizer.minimize(cost)
 
 // for d3 visualization
+// x range : [-max(x_train),max(x_train)]
+var x_ranges = _.range(101).map(
+    x=>d3.scaleLinear().domain([0,100]).range([_.max(x_train)*-1,_.max(x_train)])(x)
+)
+const maxAbsXRange = Math.ceil(_.max(x_ranges.map(value=>Math.abs(value))))
 
-var x_ranges = _.range(_.min(x_train),_.max(x_train),0.01)
+console.log(maxAbsXRange)
+console.log(["x",...x_ranges])
+
+// var chart;
+// chart.x(x_ranges)
+// console.log("chart : ",chart.x())
+ 
 // log(x_ranges)
+var canvas = document.getElementById('canvas')
 
 for (let i = 0; i < 2000; i++) {
     optimizer.minimize(()=>loss(predict(tensor_x_train),tensor_y_train));
-    if(i%1000==0){
+    if(i%100==0){
         log(`[iter ${i+1}] loss : ${loss(predict(tensor_x_train),tensor_y_train).dataSync()}`)
-        let W_pred = W.dataSync();
-        let b_pred = b.dataSync();
-        let y_preds = x_ranges.map(function(x){return W_pred*x+b_pred})
+        let W_pred = W.dataSync()[0];
+        // console.log(W_pred)
+        let b_pred = b.dataSync()[0];
+        // console.log(b_pred)        
+        let y_preds = x_ranges.map(function(x){
+            return Number(W_pred*x+b_pred).toFixed(4)
+        })
+        // console.log(["y_preds", ...y_preds])
         
-        let vis = d3.select("section").append("svg");
-        let line = d3.line()
-                    .x(function(d) { return x_ranges[d]; })
-                    .y(function(d) { return y_preds[d]; })
-        let w = 700,h = 700;
-        vis.attr("width", w)
-            .attr("height", h);
+        let newDiv = document.createElement('div')
+        newDiv.setAttribute("id", `iter${i+1}`)
+        newDiv.setAttribute("width", `250px`)
+        newDiv.setAttribute("height", `250px`)
+        newDiv.setAttribute("display", `inline`)
+        canvas.appendChild(newDiv)
+        
+        // const maxAbsYRange = Math.ceil(_.max(y_preds.map(value=>Math.abs(value))))
+        let chart = bb.generate({
+            "data": {
+                x:"x"
+                ,"columns": [
+                    ["x",...x_ranges]
+                    ,[`y_preds iter ${i+1}`,...y_preds]
+                 ]
+            }
+            ,size: {
+                "height": 200,
+                "width": 200,
+            },
+            axis: {
+                x: {
+                    tick: {
+                        count: 5
+                      }                  
+                    ,min: maxAbsXRange*-1
+                    ,max: maxAbsXRange
+                },
+                y:{
+                    tick: {
+                        count: 5
+                      }                  
+                    ,min: maxAbsXRange*-1
+                    ,max: maxAbsXRange
+                }
+            },
+            bindto: `#iter${i+1}`
+        });
     }
 }
 
