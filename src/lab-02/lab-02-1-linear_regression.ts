0e9fbc1ed719ce2fe49b338fb0b38ad7cc5a809d
brainstormot <kanghyr@gmail.com>
2018-07-19 18:04:12 +0900

    typescript porting start

---
+++ src/lab-02/lab-02-1-linear_regression.ts
@@ -0,0 +1,6 @@
+/// <reference path="../../node_modules/@tensorflow/tfjs/dist/index.d.ts" />
+import * as tf from '@tensorflow/tfjs';
+
+let a:tf.Tensor = tf.zeros([1])
+console.log(a)
+console.log("HI")
